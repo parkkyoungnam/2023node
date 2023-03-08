3c905b37d41f06b02dafe1102465bb1ebf165010
박경남 <pkn0813@naver.com>
2023-03-08 23:14:50 +0900

    CH10 _1

---
+++ 2023nodeproject/ch10/passport/kakaoStrategy.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const {Strategy : KakaoStrategy} = require('passport-kakao');
+const User = require('../models/user');
+
+module.exports = () => {
+    passport.use(new KakaoStrategy({
+        clientID : process.env.KAKAO_ID,
+        callbackURL : '/auth/kakao/callback',
+    }, async (accessToekn, refreshToken, profile, done) =>{
+        console.log('profile', profile);
+
+        try {
+            const exUser = await User.findOne({
+                where : {snsId : profile.id, provider : 'kakao'}
+            });
+            if(exUser)
+            {
+                done(null,exUser);
+            }
+            else{
+                const newUser = await User.create({
+                    email : profile._json?.kakao_account?.email,
+                    nick : profile.displayName,
+                    snsId : profile.id,
+                    provider : 'kakao' 
+                })
+            }
+        }
+        catch(error)
+        {
+            console.error(error);
+            done(error);
+        }
+    }));
+};
