e8806b4cbb3f939b0bd2f2e8761d01640744b771
박경남 <pkn0813@naver.com>
2023-03-18 23:46:06 +0900

    기초과정

---
+++ 2023gicho/gicho/ch2.js
@@ -0,0 +1,26 @@
+/*
+
+화살표 함수
+
+function add1(x,y) {
+    return x + y;
+}
+
+const add2 = (x,y) => {
+    return x+ y;
+}
+
+const add3 = (x,y) => x+y;
+
+const add4 = (x,y) => (x + y);
+
+const example = {a : 123, b: {c : 125, d: 146}}
+const a = example.a;
+const d = example.b.d;
+
+const {a, b: {d} } = example;
+
+console.log(a); -> 123
+console.log(d); -> 146
+
+*/
